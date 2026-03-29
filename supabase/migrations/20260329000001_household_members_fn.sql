d83ed3077bedff2a79c5642af8e329119c38d33e
Albingardelov <albingardelov@gmail.com>
2026-03-29 23:08:00 +0200

    db: add get_household_members RPC function

---
+++ supabase/migrations/20260329000001_household_members_fn.sql
@@ -0,0 +1,12 @@
+-- supabase/migrations/20260329000001_household_members_fn.sql
+CREATE OR REPLACE FUNCTION get_household_members(hid uuid)
+RETURNS TABLE(user_id uuid, email text)
+LANGUAGE sql
+SECURITY DEFINER
+AS $$
+  SELECT hm.user_id, u.email::text
+  FROM household_members hm
+  JOIN auth.users u ON u.id = hm.user_id
+  WHERE hm.household_id = hid
+    AND is_household_member(hid);
+$$;
